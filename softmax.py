8fb5c6b1971ad84149b1cef82036afcba2b189c1
Xiaofeng <124476214t5@qq.com>
2023-03-28 16:56:28 +0800

    softmax tune_hyperparameters

--- softmax.py
+++ softmax.py
@@ -3,6 +3,7 @@
 # @Time :2023/3/28 14:25
 # @Author :Xiaofeng
 import numpy as np
+from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 from cs231n.data_utils import preprocess_data
@@ -42,7 +43,7 @@ def tune_hyperparameters():
     ################################################################################
     X_train, y_train, X_val, y_val, X_test, y_test, X_dev, y_dev = preprocess_data()
     # Provided as a reference. You may or may not want to change these hyperparameters
-    learning_rates = [1e-7, 5e-7]
+    learning_rates = [1e-8, 3e-7]
     regularization_strengths = [2.5e4, 5e4]
     lr_range = np.linspace(learning_rates[0], learning_rates[1], 8)
     reg_range = np.linspace(regularization_strengths[0], regularization_strengths[1], 8)
@@ -67,6 +68,26 @@ def tune_hyperparameters():
                 lr, reg, acc_train, acc_val))
 
     print('best validation accuracy achieved during cross-validation: %f' % best_val)
+    # evaluate on test set
+    # Evaluate the best softmax on test set
+    y_test_pred = best_softmax.predict(X_test)
+    test_accuracy = np.mean(y_test == y_test_pred)
+    print('softmax on raw pixels final test set accuracy: %f' % (test_accuracy,))
+    # Visualize the learned weights for each class
+    w = best_softmax.W[:-1, :]  # strip out the bias
+    w = w.reshape(32, 32, 3, 10)
+
+    w_min, w_max = np.min(w), np.max(w)
+
+    classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    for i in range(10):
+        plt.subplot(2, 5, i + 1)
+
+        # Rescale the weights to be between 0 and 255
+        wimg = 255.0 * (w[:, :, :, i].squeeze() - w_min) / (w_max - w_min)
+        plt.imshow(wimg.astype('uint8'))
+        plt.axis('off')
+        plt.title(classes[i])
 
 
 if __name__ == '__main__':
