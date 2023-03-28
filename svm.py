6c0b11f858652a4e030376c09736d2eec3f8dfa8
Xiaofeng <124476214t5@qq.com>
2023-03-28 14:07:04 +0800

    softmax learning

--- svm.py
+++ svm.py
@@ -14,57 +14,37 @@ from cs231n.classifiers.linear_svm import svm_loss_vectorized
 import time
 
 
-def preprocess_data():
+def preprocess_data(num_training=49000, num_validation=1000, num_test=1000, num_dev=500):
     """
-    预处理数据
-    Returns:
-        -X_train, y_train: training set,
-        -X_val,   y_val  : validation set,
-        -X_test,  y_test : test set,
-        -X_dev,   y_dev  : development set.
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for the linear classifier. These are the same steps as we used for the
+    SVM, but condensed to a single function.
     """
     # Load the raw CIFAR-10 data.
     cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+    # # Cleaning up variables to prevent loading data multiple times (which may cause memory issue)
+    # try:
+    #     del X_train, y_train
+    #     del X_test, y_test
+    #     print('Clear previously loaded data.')
+    # except:
+    #     pass
+
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
-    # Split the data into train, val, and test sets. In addition we will
-    # create a small development set as a subset of the training data;
-    # we can use this for development so our code runs faster.
-    num_training = 49000
-    num_validation = 1000
-    num_test = 1000
-    num_dev = 500
-
-    # Our validation set will be num_validation points from the original
-    # training set.
-    mask = range(num_training, num_training + num_validation)
+    # subsample the data
+    mask = list(range(num_training, num_training + num_validation))
     X_val = X_train[mask]
     y_val = y_train[mask]
-
-    # Our training set will be the first num_train points from the original
-    # training set.
-    mask = range(num_training)
+    mask = list(range(num_training))
     X_train = X_train[mask]
     y_train = y_train[mask]
-
-    # We will also make a development set, which is a small subset of
-    # the training set.
+    mask = list(range(num_test))
+    X_test = X_test[mask]
+    y_test = y_test[mask]
     mask = np.random.choice(num_training, num_dev, replace=False)
     X_dev = X_train[mask]
     y_dev = y_train[mask]
-
-    # We use the first num_test points of the original test set as our
-    # test set.
-    mask = range(num_test)
-    X_test = X_test[mask]
-    y_test = y_test[mask]
-
-    # print('Train data shape: ', X_train.shape)
-    # print('Train labels shape: ', y_train.shape)
-    # print('Validation data shape: ', X_val.shape)
-    # print('Validation labels shape: ', y_val.shape)
-    # print('Test data shape: ', X_test.shape)
-    # print('Test labels shape: ', y_test.shape)
 
     # Preprocessing: reshape the image data into rows
     X_train = np.reshape(X_train, (X_train.shape[0], -1))
@@ -72,36 +52,19 @@ def preprocess_data():
     X_test = np.reshape(X_test, (X_test.shape[0], -1))
     X_dev = np.reshape(X_dev, (X_dev.shape[0], -1))
 
-    # As a sanity check, print out the shapes of the data
-    # print('Training data shape: ', X_train.shape)
-    # print('Validation data shape: ', X_val.shape)
-    # print('Test data shape: ', X_test.shape)
-    # print('dev data shape: ', X_dev.shape)
-
-    # Preprocessing: subtract the mean image
-    # first: compute the image mean based on the training data
+    # Normalize the data: subtract the mean image
     mean_image = np.mean(X_train, axis=0)
-    # print(mean_image[:10])  # print a few of the elements
-    # plt.figure(figsize=(4, 4))
-    # plt.imshow(mean_image.reshape((32, 32, 3)).astype('uint8'))  # visualize the mean image
-    # plt.show()
-
-    # second: subtract the mean image from train and test data
     X_train -= mean_image
     X_val -= mean_image
     X_test -= mean_image
     X_dev -= mean_image
 
-    # third: append the bias dimension of ones (i.e. bias trick) so that our SVM
-    # only has to worry about optimizing a single weight matrix W.
-    # np.ones()函数返回给定形状和数据类型的新数组，其中元素的值设置为1
-    # np.hstack()将参数元组的元素数组按水平方向进行叠加
+    # add bias dimension and transform into columns
     X_train = np.hstack([X_train, np.ones((X_train.shape[0], 1))])
     X_val = np.hstack([X_val, np.ones((X_val.shape[0], 1))])
     X_test = np.hstack([X_test, np.ones((X_test.shape[0], 1))])
     X_dev = np.hstack([X_dev, np.ones((X_dev.shape[0], 1))])
-    # 结果是多加了一列1 (49000,3072)->(49000,3073)
-    # print(X_train.shape, X_val.shape, X_test.shape, X_dev.shape)
+
     return X_train, y_train, X_val, y_val, X_test, y_test, X_dev, y_dev
 
 
